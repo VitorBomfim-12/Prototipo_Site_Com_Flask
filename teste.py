b196749fdfbac99a717338e2f23e7ec7542d3bf2
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-10-30 09:58:11 -0300

    chore: reestruturação do requirements.txt, desrastreio do teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-from projeto import database,app
-from projeto.models import Equipamento,Chamado
-
-with app.app_context():
-    equipamentos_tupla = database.session.query(Chamado.equipamento_id).all()
-
-lista_defeitos = [equipamento_id for (equipamento_id,) in equipamentos_tupla]
-
-print(lista_defeitos)
